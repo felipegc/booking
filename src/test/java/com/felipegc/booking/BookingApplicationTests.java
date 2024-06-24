86022faa5dcd6f67ef06f5176f2874b104a26745
felipegc <felipecastro07@gmail.com>
2024-06-24 17:40:41 -0300

    adding mvn springboot base project

---
+++ src/test/java/com/felipegc/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipegc.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
